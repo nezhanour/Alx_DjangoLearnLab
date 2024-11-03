59e1d871f352327617df41c0db1cdd3b1f836317
nezhanour <nezha95nour@gmail.com>
2024-11-03 19:30:57 +0100

    Added list filters and search capabilities to Admin Interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Book
 
-admin.site.register(Book)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author', 'publication_year')
+    list_filter = ('title', 'author', 'publication_year')
