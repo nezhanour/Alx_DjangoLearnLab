17610707cebf2f641e2f500192d4d7836a674de1
nezhanour <nezha95nour@gmail.com>
2024-11-10 19:49:00 +0100

    removed spaces to pass cheaker

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 # Query all books by a specific author
 def books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = Book.objects.filter(author = author)
+    books = Book.objects.filter(author=author)
     return books
 
 # List all books in a library
