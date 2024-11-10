7d96725682c7365f3b2990e072ec8c9d06c70e94
nezhanour <nezha95nour@gmail.com>
2024-11-10 22:43:48 +0100

    Added admin_view, librarian_view, and member_view

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
+
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
 ]
