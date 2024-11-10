dc1f2947e6a2b962cec9b2e2b91b4598cc78bab7
nezhanour <nezha95nour@gmail.com>
2024-11-10 20:50:54 +0100

    Created a class-based view that displays details for a specific library, listing all books available in that library and an HTML template for Displaying

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import Book
+from django.views.generic import DetailView
+from .models import Library
+
 
 # Function-based view to list all books and their authors
 def book_list(request):
     books = Book.objects.all()
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
+
+# Class-based view to display details of a specific library and its books
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
