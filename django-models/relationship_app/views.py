61f8ec005af001e9601af14e8f3f7fa16e527ae2
nezhanour <nezha95nour@gmail.com>
2024-11-10 22:34:49 +0100

    Setted Up Role-Based Views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import user_passes_test
 
 
 # Function-based view to list all books and their authors
@@ -31,3 +32,29 @@ def register(request):
         form = UserCreationForm()
     
     return render(request, 'relationship_app/register.html', {'form': form})
+
+
+# Role-checking functions
+def is_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+# View for Admin users
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+# View for Librarian users
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+# View for Member users
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
