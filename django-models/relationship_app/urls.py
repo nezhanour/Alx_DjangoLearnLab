88486b7a1bd6526f60e33f65e7efcf2870feb25b
nezhanour <nezha95nour@gmail.com>
2024-11-10 22:11:44 +0100

    Added Login Template, Logout Template, and Registration Template

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import list_books, LibraryDetailView
-from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import  LogoutView
 from . import views
 
 
@@ -12,5 +12,5 @@ urlpatterns = [
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
     path('login/', views.register(template_name='login.html'), name='login'),
-    path('logout/', views.register(template_name='logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
 ]
