d932c3fc73ff1a6e66e263dfb5d31c26e86ff82f
nezhanour <nezha95nour@gmail.com>
2024-11-10 20:36:02 +0100

    Created a function-based view that lists all books stored in the database, and an HTML Template for Listing Books

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+# Function-based view to list all books and their authors
+def book_list(request):
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
