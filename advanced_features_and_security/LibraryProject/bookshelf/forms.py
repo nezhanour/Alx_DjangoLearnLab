72f63d83bf2768362167f26b3efd44c84d7d0d05
nezhanour <nezha95nour@gmail.com>
2024-11-17 20:59:24 +0100

    Protectd Views with CSRF Tokens and Secured Data Access in Views

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,4 @@
+from django import forms
+
+class ExampleForm(forms.Form):
+    pass
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -7,3 +7,13 @@ def edit_book(request, title):
     book_list = get_object_or_404(Book, title=title)
     # Perform the edit logic here
     return render(request, 'bookshelf/edit_book.html', {'books': book_list})
+
+def search_books(request):
+    query = request.GET.get('q', '')
+    if query:
+        # Securely filter books using Django ORM
+        books = Book.objects.filter(title__icontains=query)
+    else:
+        books = Book.objects.all()
+    
+    return render(request, 'bookshelf/book_list.html', {'books': books})
