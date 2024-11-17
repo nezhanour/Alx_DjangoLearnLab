99b692bfd5b441fc25907fa0826e053077d43e03
nezhanour <nezha95nour@gmail.com>
2024-11-17 17:28:51 +0100

    Added CustomUserAdmin class in admin.py

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import Book
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + ((None, {'fields': ('date_of_birth', 'profile_photo')}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {'fields': ('date_of_birth', 'profile_photo')}),)
+
+admin.site.register(CustomUser, CustomUserAdmin)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
