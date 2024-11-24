d2fec862c2a2d947f256543c746893381fd26929
nezhanour <nezha95nour@gmail.com>
2024-11-24 15:26:35 +0100

    Created a ViewSet

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,12 @@
 from rest_framework import generics
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework import viewsets
 
-class BookList(generics.ListAPIView):
+#class BookList(generics.ListAPIView):
+#    queryset = Book.objects.all()
+#    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
