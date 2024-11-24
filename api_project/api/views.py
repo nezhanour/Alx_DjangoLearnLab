d08ecb6bb473b6c9ffc789621b3710f06bc8460c
nezhanour <nezha95nour@gmail.com>
2024-11-24 15:56:46 +0100

    Setted a Router

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -3,9 +3,9 @@ from .serializers import BookSerializer
 from .models import Book
 from rest_framework import viewsets
 
-#class BookList(generics.ListAPIView):
-#    queryset = Book.objects.all()
-#    serializer_class = BookSerializer
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
