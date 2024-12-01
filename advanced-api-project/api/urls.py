ea141c51c01f43a4435d6e1641e6dab806135819
nezhanour <nezha95nour@gmail.com>
2024-12-01 19:11:47 +0100

    updated url parameters

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/', ListView.as_view(), name='books-list'),
     path('books/<int:pk>/', DetailView.as_view(), name='books-retrieve'),
     path('books/create', CreateView.as_view(), name='books-create'),
-    path('books/<int:pk>/update', UpdateView.as_view(), name='books-update'),
-    path('books/<int:pk>/delete', DeleteView.as_view(), name='books-delete'),
+    path('books/update', UpdateView.as_view(), name='books-update'),
+    path('books/delete', DeleteView.as_view(), name='books-delete'),
 ]
