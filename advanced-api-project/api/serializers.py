c524e0b5a811f88ff053d31add753cab3f8681f3
nezhanour <nezha95nour@gmail.com>
2024-12-01 12:39:01 +0100

    Created a custom validation to the BookSerializer

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -1,15 +1,22 @@
 from .models import Book, Author
 from rest_framework import serializers
+from datetime import date
 
-class BookSerializer(serializers.Serializer):
+class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
         fields = ['title', 'publication_year', "author"]
 
-class AuthorSerializer(serializers.Serializer):
+    def Validate(self, data):
+        datenow = date.today()
+        if 'publication_year' in data:
+            if data['publication_year'] > datenow:
+                raise serializers.ValidationError('Puplication date cannot be in the Future')
+        return super(BookSerializer, self).validate(data)
+
+class AuthorSerializer(serializers.ModelSerializer):
+    books = BookSerializer(many=True, read_only=True)
+
     class Meta:
         model = Author
-        fields = ["name"]
-
-    def Validate(self):
-        pass
+        fields = ['name', 'books']
