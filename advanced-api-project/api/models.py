55a2576cd433d2768b436936476575aed74031bb
nezhanour <nezha95nour@gmail.com>
2024-12-01 12:42:33 +0100

    added detailed comments

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+#Creation of the Author of book model 
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
+#Creation of Book model with  a one-to-many relationship from Author to Books
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_year = models.DateField()
