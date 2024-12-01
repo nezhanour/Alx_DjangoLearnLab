80228c61b4f2d0116ce9f2e3897f467def12e1ca
nezhanour <nezha95nour@gmail.com>
2024-12-01 19:17:52 +0100

    Implemented Permissions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,23 +2,29 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 class ListView(ListAPIView):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all() # All books in the database
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class DetailView(RetrieveAPIView):
-    queryset = Book.objects.all()
+    queryset = Book.objects.all() # All books in the database
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CreateView(CreateAPIView):
     queryset = Book.objects.all() # All books in the database
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class UpdateView(UpdateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 class DeleteView (DestroyAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
