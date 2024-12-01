c8df2e2c8f74fe11abaffc94a723e9b5ff231656
nezhanour <nezha95nour@gmail.com>
2024-12-01 20:36:35 +0100

    Implemented Filtering, Search Functionality and Configured Ordering

---
+++ advanced-api-project/api/README.md
@@ -0,0 +1,13 @@
+API Query Features:
+
+Filtering:
+Filter books by title, author’s name, or publication year.
+Example: /books/?title=Django&publication_year=2023
+
+Searching:
+Search for books by title or author’s name.
+Example: /books/?search=John
+
+Ordering:
+Order books by title or publication year (ascending or descending).
+Example: /books/?ordering=-publication_year
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,11 +3,20 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView,
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 
 class ListView(ListAPIView):
     queryset = Book.objects.all() # All books in the database
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    # Filter by title, author's name, and publication year
+    filterset_fields = ['title', 'author', 'publication_year']
+    # Search by book title or author name
+    search_fields = ['title', "author__name"]
+    # Allow ordering by title and publication year
+    ordering_fields = ['title', 'publication_year']
 
 class DetailView(RetrieveAPIView):
     queryset = Book.objects.all() # All books in the database
