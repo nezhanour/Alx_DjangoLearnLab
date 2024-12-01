335b4ca3fa33aa4d40dff45f0592b6b190d853ee
nezhanour <nezha95nour@gmail.com>
2024-12-01 12:46:50 +0100

    added detailed comments

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -2,6 +2,7 @@ from .models import Book, Author
 from rest_framework import serializers
 from datetime import date
 
+#A BookSerializer that serializes all fields of the Book model with a custom validation to ensure the publication_year is not in the future.
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
@@ -14,6 +15,7 @@ class BookSerializer(serializers.ModelSerializer):
                 raise serializers.ValidationError('Puplication date cannot be in the Future')
         return super(BookSerializer, self).validate(data)
 
+#An AuthorSerializer that serializes all fields of the Author mode with a nested BookSerializer to serialize the related books dynamically
 class AuthorSerializer(serializers.ModelSerializer):
     books = BookSerializer(many=True, read_only=True)
 
