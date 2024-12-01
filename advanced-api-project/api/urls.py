a84e917c08090a87c6b2b3a911c8ed02f4321c8b
nezhanour <nezha95nour@gmail.com>
2024-12-01 17:53:11 +0100

    Created Generic Views (list view, detail viewand create view)

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ListView
+from .views import ListView, DetailView, CreateView
 
 urlpatterns = [
-    path('books/', ListView.as_view(), name='books-list')
+    path('books/', ListView.as_view(), name='books-list'),
+    path('books/<pk>/', DetailView.as_view(), name='books-retrieve'),
+    path('books/create', CreateView.as_view(), name='books-create')
 ]
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, CreateAPIView
 from .models import Book
 from .serializers import BookSerializer
 
 class ListView(ListAPIView):
-    queryset = Book
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class DetailView(RetrieveAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CreateView(CreateAPIView):
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
