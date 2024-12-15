9f9b00092d62950d21acac509ff301f8b0266a10
nezhanour <nezha95nour@gmail.com>
2024-12-15 19:08:58 +0100

    Created views for user registration, login

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
 
-# Create your views here.
+User = get_user_model()
+
+class RegisterUser(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token, created = Token.objects.get_or_create(user=user)
+            return Response(serializer.errors, status=400)
+        
+class loginUser(ObtainAuthToken):
+    permission_classes = [AllowAny]
