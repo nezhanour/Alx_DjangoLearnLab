b85390af769876cf3de643bc671aeff0b60b3eba
nezhanour <nezha95nour@gmail.com>
2024-12-15 21:58:19 +0100

    Updated the User Model to Handle Follows and Created API Endpoints for Managing Follows

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
-    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following_me')
+    following = models.ManyToManyField('self', symmetrical=False, related_name='my_followers', blank=True)
 
     def __str__(self):
         return self.username
--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import RegisterUserView, loginUserView, UserProfileView
+from .views import RegisterUserView, loginUserView, UserProfileView, FollowUser, UnfollowUser
 
 urlpatterns = [
     path('register/', RegisterUserView.as_view(), name='register'),
     path('login/', loginUserView.as_view(), name='login'),
     path('profile/', UserProfileView.as_view(), name='profile'),
+    path('follow/<int:user_id>/', FollowUser.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUser.as_view(), name='unfollow-user'),
 ]
