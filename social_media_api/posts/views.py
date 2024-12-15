7cdd8edc45b69241c8ce4a9231baad011b6cff8d
nezhanour <nezha95nour@gmail.com>
2024-12-15 22:14:17 +0100

    Fixed the error in posts/views FeedView class

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -48,7 +48,7 @@ class FeedView(APIView):
 
     def get(self, request):
         # Get posts from followed users
-        followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        following_users = request.user.following.all()
+        posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serializer = PostSerializer(posts, many=True)
         return Response(serializer.data)
