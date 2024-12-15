e4d34a7ee34e816d3be7823a07862603aac6ee4f
nezhanour <nezha95nour@gmail.com>
2024-12-15 20:56:26 +0100

    Fixed UserRegistrationSerializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -11,7 +11,7 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
             password=validated_data['password'],
